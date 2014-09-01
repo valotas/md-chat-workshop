0bd6ffc2aaf2da9ec159015492bc38e99ca6ec2e
Gion Kunz <gion.kunz@ctp.com>
2014-09-01 22:52:47 +0200

    Added basic login service with  broadcast for events

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -4,7 +4,33 @@
   // Our main application module
   angular.module('chatApp', [])
 
-    .controller('ChatCtrl', function ($scope) {
+    .factory('chatService', function ($rootScope) {
+      var connected = false;
+
+      // Login service function
+      function login() {
+        connected = true;
+        $rootScope.$broadcast('connected');
+      }
+
+      // Logout function that disconnects all firebase connections and updates the internal state
+      function logout() {
+        connected = false;
+        $rootScope.$broadcast('disconnected');
+      }
+
+      function isLoggedIn() {
+        return connected;
+      }
+
+      return {
+        login: login,
+        logout: logout,
+        isLoggedIn: isLoggedIn
+      };
+    })
+
+    .controller('ChatCtrl', function ($scope, chatService) {
       // Initialize controller variables
       $scope.messageText = '';
       $scope.credentials = {
@@ -14,15 +40,22 @@
 
       // Controller login and logout function
       $scope.login = function () {
-        $scope.isLoggedIn = true;
+        chatService.login($scope.credentials);
       };
       $scope.logout = function () {
         // Close any offcanvas first
         $('.off-canvas-wrap').removeClass('move-right');
-        $scope.isLoggedIn = false;
+        chatService.logout();
       };
 
+      // isLoggedIn handling by observing the connected and disconnected events from chatService
       $scope.isLoggedIn = false;
+      $scope.$on('connected', function () {
+        $scope.isLoggedIn = true;
+      });
+      $scope.$on('disconnected', function () {
+        $scope.isLoggedIn = false;
+      });
     });
 
 }(window, jQuery));
