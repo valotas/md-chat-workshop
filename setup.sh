f47e8d3fd1578e2a93ce7c21d6af421419e14d34
Gion Kunz <gion.kunz@ctp.com>
2014-09-06 00:25:05 +0200

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ ! -d $HOME/md-chat-workshop/.git ]
+then
+  cd $HOME
+  git clone https://github.com/gionkunz/md-chat-workshop.git
+  cd $HOME/md-chat-workshop
+  for remote in `git branch -r | grep -v master `; do git checkout --track $remote ; done
+  git checkout master
+else
+  cd $HOME/md-chat-workshop
+  git pull-all
+fi
+ 
+cd $HOME/md-chat-workshop
+npm install
+bower install
+
